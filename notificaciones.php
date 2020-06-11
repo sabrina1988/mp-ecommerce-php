f0d89f021ee533117d637c2625c36d1d0139c271
sabri23 <sabrina.cuevas23@gmail.com>
2020-06-11 14:19:51 -0300

    -

--- notificaciones.php
+++ notificaciones.php
@@ -33,7 +33,7 @@ error_log("===========  REQUEST  ============== ".print_r($_REQUEST, true));
 $entityBody = file_get_contents('php://input');
 error_log("===========  TODO  ============== ".print_r($entityBody, true));
 
- $archivo = fopen('ipn.log','a'); 
+ $archivo = fopen('info.log','a'); 
  fwrite($archivo,"[".date("r")."] DATA:  $_POST\r\n"); 
  fclose($ddf); 
 
