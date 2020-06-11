fc0898952353e0b81d9dc608c144b4d8ee17a1b2
sabri23 <sabrina.cuevas23@gmail.com>
2020-06-11 17:09:39 -0300

    -

--- detail.php
+++ detail.php
@@ -3,10 +3,10 @@
     // SDK de Mercado Pago
     require __DIR__ .  '/vendor/autoload.php';
                     
-    $access_token   =   "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398";
+    //$access_token   =   "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398";
 
     // Agrega credenciales
-    MercadoPago\SDK::setAccessToken($access_token);
+    MercadoPago\SDK::setAccessToken($_ENV['access_token']);
 
     // Crea un objeto de preferencia
     $preference = new MercadoPago\Preference();   
