e3dae9750a1d49e5ab30eb455930cbd4690a5aab
sabrina1988 <sabrina.cuevas23@gmail.com>
2020-06-11 09:56:45 -0300

    -

--- notificaciones.php
+++ notificaciones.php
@@ -4,8 +4,8 @@ require './vendor/autoload.php';
 
 MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 
-if($_POST["type"]){
-    switch($_POST["type"]) {
+if($_POST["id"]){
+    switch($_POST["topic"]) {
         case "payment":
             $response = MercadoPago\Payment.find_by_id($_POST["id"]);
             break;
@@ -17,14 +17,13 @@ if($_POST["type"]){
             break;
         case "invoice":
             $response = MercadoPago\Invoice.find_by_id($_POST["id"]);        
-            break;
-    
-        
+            break;        
     }
+    
     http_response_code(200);
     return;
 }else{
-    http_response_code(200);
+    http_response_code(400);
     return;
 }
 
