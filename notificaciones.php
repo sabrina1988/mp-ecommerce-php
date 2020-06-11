81c57ce2e96898fee476a58832b53ac8a27a01fb
sabrina1988 <sabrina.cuevas23@gmail.com>
2020-06-11 01:58:03 -0300

    -

--- notificaciones.php
+++ notificaciones.php
@@ -4,33 +4,19 @@ MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a
 
 switch($_POST["type"]) {
     case "payment":
-        $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
-
-        $archivo = fopen('json_notificacion.txt','a');        
-        fputs($archivo,$payment);
-        fclose($archivo);
+        $response = MercadoPago\Payment.find_by_id($_POST["id"]);
         break;
     case "plan":
-        $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
-        
-        $archivo = fopen('json_notificacion.txt','a');
-        fputs($archivo,$plan);
-        fclose($archivo);
+        $response = MercadoPago\Plan.find_by_id($_POST["id"]);        
         break;
     case "subscription":
-        $subscription = MercadoPago\Subscription.find_by_id($_POST["id"]);
-        
-        $archivo = fopen('json_notificacion.txt','a');
-        fputs($archivo,$subscription);
-        fclose($archivo);
+        $response = MercadoPago\Subscription.find_by_id($_POST["id"]);        
         break;
     case "invoice":
-        $invoice = MercadoPago\Invoice.find_by_id($_POST["id"]);
-        
-        $archivo = fopen('json_notificacion.txt','a');
-        fputs($archivo,$invoice);
-        fclose($archivo);
+        $response = MercadoPago\Invoice.find_by_id($_POST["id"]);        
         break;
+
+    echo $response;
 }
 
 ?>
