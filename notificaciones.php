e1d8e0cbcb197e42b3f52f9447f99d6b581bf449
sabrina1988 <sabrina.cuevas23@gmail.com>
2020-06-11 04:17:40 -0300

    -

--- notificaciones.php
+++ notificaciones.php
@@ -2,7 +2,7 @@
 
 MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 
-if(isset($_POST["type"])){
+if($_POST["type"]){
     switch($_POST["type"]) {
         case "payment":
             $response = MercadoPago\Payment.find_by_id($_POST["id"]);
