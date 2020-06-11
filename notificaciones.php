d0483208882f3eba9d9da87e1232ee8a8076cc1c
sabrina1988 <sabrina.cuevas23@gmail.com>
2020-06-11 04:24:56 -0300

    -

--- notificaciones.php
+++ notificaciones.php
@@ -1,6 +1,6 @@
 <?php
 
-require '../vendor/autoload.php';
+require './vendor/autoload.php';
 
 MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 
